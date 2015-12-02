c8516c5c67867cc8306210969c0347d2c2e9db6c
Lucas Correa <lucas.correa@itec.ufpa.br>
2015-12-02 14:17:57 -0300

    correcao na leitura da serial na funcao  main

--- uart/uart.c
+++ uart/uart.c
@@ -14,13 +14,13 @@ void uart_init (void)
 	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
 }
 
-char uart_getchar (void)
+unsigned char uart_getchar (void)
 {
 	while (!(UCSR0A & (1 << RXC0))); //aguarda chegar algum dado
 	return UDR0; //faz a leitura dos dados 
 }
 
-void uart_putchar (const char c)
+void uart_putchar (const unsigned char c)
 {
 	while (!(UCSR0A & (1 << UDRE0))); //aguarda a transmissao de algum dado anterior
 	UDR0 = c; //escreve dados no registrador (envia o dado)
@@ -41,8 +41,10 @@ int main (void)
 		uart_putchar ('\r');
 		uart_putchar ('\n');
 
-		while (read = uart_getchar() != '\n')
+		while ((read = uart_getchar()) != '\n')
 			uart_putchar(read);
+
+		uart_putchar(read);
 
 		PORTB ^= (1<<PB5);
 	}
